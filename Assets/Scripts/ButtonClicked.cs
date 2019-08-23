831266c98d741c6a141be8e6d658b89800d0fad8
MisterShake2099 <mistershake2099@gmail.com>
2019-08-23 03:49:11 -0700

    Some messing around with UI stuff.

---
+++ Assets/Scripts/ButtonClicked.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ButtonClicked : MonoBehaviour
+{
+	public void ButtonWasClicked()
+	{
+		Debug.Log("The button was clicked.");
+	}
+
+	public void ButtonWasClickedWithParameter(string parameter)
+	{
+		string message = string.Format("The button was clicked: {0}", parameter);
+		Debug.Log(message);
+	}
+
+	void PrivateButtonWasClicked()
+	{
+		Debug.Log("This won't run as the direct result of a button click!");
+	}
+}
