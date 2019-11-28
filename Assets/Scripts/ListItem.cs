c43ec269314473ecdb43620dee706a83b7390def
MisterShake2099 <mistershake2099@gmail.com>
2019-11-28 03:40:18 -0800

    Scrolling, auto-populating list example.

---
+++ Assets/Scripts/ListItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ListItem : MonoBehaviour
+{
+    [SerializeField] Text labelText;
+
+    public string Label
+    {
+        get
+        {
+            return labelText.text;
+        }
+        set
+        {
+            labelText.text = value;
+        }
+    }
+
+}
