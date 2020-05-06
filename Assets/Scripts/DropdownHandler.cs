8adc3c2592cb2e775d165f254562056963298fc7
MisterShake2099 <mistershake2099@gmail.com>
2020-05-06 12:47:34 -0700

    Clean up some junk assets.

---
+++ Assets/Scripts/DropdownHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownHandler : MonoBehaviour
+{
+    public Text TextBox;
+
+    void Start()
+    {
+        var dropdown = transform.GetComponent<Dropdown>();
+
+        dropdown.options.Clear();
+
+        List<string> items = new List<string>
+        {
+            "Item 1",
+            "Item 2"
+        };
+
+        foreach (var item in items)
+        {
+            dropdown.options.Add(new Dropdown.OptionData() { text = item });
+        }
+
+        DropdownItemSelected(dropdown);
+        dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
+    }
+
+    void DropdownItemSelected(Dropdown dropdown)
+    {
+        int index = dropdown.value;
+        TextBox.text = dropdown.options[index].text;
+    }
+
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+
+    void Start()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+}
